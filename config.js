4044606b402d75d82624efa36d81d694d8876c19
lucasnvs <lucasli.nev@hotmail.com>
2023-08-17 18:49:26 -0300

    Init

---
+++ config.js
@@ -0,0 +1,26 @@
+// export const config = { // chico | Crips
+//     TOKEN: "NTc1NDIxNTY4MDA4NzE2MzA3.GGcBbm.yGpQtUmMbHI_sN-OJrBOUx2VzD4kyBAti2l8RI",
+//     ID_CHAT_BAU: "1083484014389772439",
+//     LAST_ID_MESSAGE: "1140473505255805071",
+//     ITEM_SEARCHED: "Peça de Arma" 
+// }
+
+export const config = { // lucasnvs | Ballas
+    TOKEN: "NTgxNjYzNjg3NzM2Mjk1NDU0.GDyeEH.pwmfmqAVvGNqiui17lkV8N-ecr9fYMeooY5SGI",
+    ID_CHAT_BAU: "1083483431238905896",
+    LAST_ID_MESSAGE: "1140494558426300488",
+    ITEM_SEARCHED: "Folha de Coca" 
+}
+
+export const seeAll = true;
+
+export const personIdName = "417 Vitor Souza";
+
+// 617 Felipe Alves ExtraGG Scamander
+// 3028 Matheus Triunfo
+// 101 Diogo Contijo
+// 2282 Sasuke Castello
+// 11199 RAFAEL SANTOS
+// 800 Jorgin DaDOZE
+// 8803 joao pedro   // <== tue
+// 12993 Livinho daKakah // 3000
